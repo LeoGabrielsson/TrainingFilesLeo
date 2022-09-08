2c86a56297cf0bf4c803b76541eb4a35406642f0
TyrkiskPeber <77719459+TyrkiskPeber@users.noreply.github.com>
2022-09-08 13:51:15 +0200

    Initial code to test

--- src/Filhantering.java
+++ src/Filhantering.java
@@ -1,5 +1,22 @@
+import java.io.*;
+
 public class Filhantering {
     public static void main(String[] args) {
-
+        String filnamn = "minfil.txt";
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(filnamn);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter utFil = new PrintWriter(bw);
+        FileReader fr = null;
+        try {
+            fr = new FileReader(filnamn);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader inFil = new BufferedReader(fr);
     }
 }
